aeeef8ce5a896b7939b74917a1b7ceccbab9426c
jhn221 <98151533+jhn221@users.noreply.github.com>
2022-05-13 16:34:27 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = 500;
+canvas.height = 500;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -20,6 +20,7 @@ function startPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    console.log (x,y);
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x, y);
